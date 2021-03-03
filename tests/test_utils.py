56c0602614206365ef7c9c2f3ee7f5962a2f5050
Alison Major <alison.major@gmail.com>
2021-03-02 19:33:33 -0500

    Test bad value

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@ from utilities.utils import sum
 import pytest
 
 @pytest.mark.parametrize('list_of_integers, result', [
-    ([1],1)
+    ([1],2)
 ])
 def test_sum(list_of_integers, result):
     assert sum(list_of_integers) == result
