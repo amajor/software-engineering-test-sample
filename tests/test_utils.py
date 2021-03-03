21c2b3d894acf0359b8fd5e8d1392bf674c9cd65
Alison Major <alison.major@gmail.com>
2021-03-02 20:38:20 -0500

    Set up exception testing

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,37 @@ from utilities.utils import sum
 import pytest
 
 @pytest.mark.parametrize('list_of_integers, result', [
-    ([1],2)
+    ([1],1)
 ])
 def test_sum(list_of_integers, result):
     assert sum(list_of_integers) == result
+
+###########################
+# Test Exception Messages #
+###########################
+
+def catch_exception(bad_input, expected_exception_phrase):
+    with pytest.raises(Exception, match=r".*" + expected_exception_phrase + ".*"):
+        sum(bad_input)
+
+# Test input is only list of integers
+@pytest.mark.parametrize('bad_input', [
+    ('string of some kind'),
+    (1),
+    (-1),
+    (0),
+    (2.34)
+])
+def test_only_list(bad_input):
+    expected_exception_phrase = "Input type 'list' only."
+    catch_exception(bad_input, expected_exception_phrase)
+
+# Test input is only list of positive integers
+@pytest.mark.parametrize('bad_input', [
+    # ([1.2, 3.40]),
+    # (['a','b','c']),
+    ([-1])
+])
+def test_only_positive_integers(bad_input):
+    expected_exception_phrase = "Positive integers only."
+    catch_exception(bad_input, expected_exception_phrase)
--- utilities/utils.py
+++ utilities/utils.py
@@ -1,2 +1,9 @@
 def sum(list_of_integers):
-    return list_of_integers[0]
+    if isinstance(list_of_integers, list):
+        if list_of_integers[0] < 0:
+            raise Exception("Positive integers only.")
+        sum_of_integers = list_of_integers[0]
+    else:
+        raise Exception("Input type 'list' only.")
+
+    return sum_of_integers
